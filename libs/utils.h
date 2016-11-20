3077f18d959fcf9f674069cc0f757230132996d4
RicardoBarbosa <ricardopbarbosaa@gmail.com>
2016-11-20 23:57:14 +0000

    Manual insert all the blocks for each hand, read and write on files both text and binary, edit blocks from the hands

--- libs/utils.h
+++ libs/utils.h
@@ -5,27 +5,33 @@
 #ifndef DOMINO_PROJECT_UTILS_H
 #define DOMINO_PROJECT_UTILS_H
 
-#define MAX2 2
+#define COLS 2
 #define LINES 28
 
-int getGame(int pieces[][MAX2]);
+int getGame(int pieces[][COLS]);
 void printHandVertically(int size);
-void printHandHorizontally(int hand[][MAX2], int size, int index);
-void printHand_uglify(int hand[][MAX2], int size, int index);
+void printHandHorizontally(int hand[][COLS], int size, int index);
+void printHand_uglify(int hand[][COLS], int size, int index);
 int printMenu(int path);
-void generateRandomHand(int matrix[][MAX2], int hand[][MAX2], int linesHand, int qtSet);
-void compressMatrix(int matrix[][MAX2], int lines, int index);
-void printMat(int matrix[][MAX2], int lines);
-void inittMat(int m[][MAX2], int lines);
+void generateRandomHand(int matrix[][COLS], int hand[][COLS], int linesHand, int qtSet);
+void compressMatrix(int matrix[][COLS], int lines, int index);
+void printMat(int matrix[][COLS], int lines);
+int blocksAvailable(int matrix[][COLS]);
+void generateManualHand(int matrix[][COLS], int hand[][COLS], int linesHand, int qtSet);
+int editHands(int matrix[][COLS], int hand[][COLS], int handSize, int numberOfHands);
+void inittMat(int m[][COLS], int lines);
 
-int generateSequence(int matrix[][MAX2], int handSize, int sequence[][MAX2], int inserted) ;
-int isConsistent(int sequence[][MAX2], int index);
-void invertBlock(int sequence[][MAX2], int index);
-void changeBlock(int mat[][MAX2], int index, int inserted);
+int generateSequence(int matrix[][COLS], int handSize, int sequence[][COLS], int inserted) ;
+int isConsistent(int sequence[][COLS], int index);
+void invertBlock(int sequence[][COLS], int index);
+void changeBlock(int mat[][COLS], int index, int inserted);
 
-//int fileExists(const char *filename);
-void openFile(int type, int aux[LINES][MAX2], int *numberOfHands, int *handSize);
-void createGameFile(int content[LINES][MAX2], int numberOfHands, int handSize);
-void checkExtension(char *fileName);
+void openFile(char fileName[], int type, int handsMat[LINES][COLS], int gameMat[LINES][COLS], int *numberOfHands, int *handSize);
+void editFile(char fileName[], int type, int handsMat[LINES][COLS], int gameMat[LINES][COLS], int numberOfHands, int handSize);
+void createGameFile(int type, int handsMat[LINES][COLS], int gameMat[LINES][COLS], int numberOfHands, int handSize);
+void checkExtension(char fileName[], char extension[]);
+int fileExists(char fileName[]);
+int toLower(char c);
+int isValid(int input, int validationType);
 
 #endif //DOMINO_PROJECT_UTILS_H
